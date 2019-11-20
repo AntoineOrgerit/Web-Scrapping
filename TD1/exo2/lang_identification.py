a7f8aa4168e6ef169a6784bdf7e656f6aecd3c8c
Antoine Orgerit <orgerit.antoine@gmail.com>
2019-11-20 10:49:06 +0100

    Generating trueLg and langid files by adapting relative paths.

--- TD1/exo2/lang_identification.py
+++ TD1/exo2/lang_identification.py
@@ -4,22 +4,26 @@ from iso639 import languages
 from utils.extractor import extract
 
 def jt_langid_treatement(input_file, output_file):
-    language = langid.classify(input_file.read())
-    language = languages.get(alpha2=language[0]).name
-    if "Greek" in language:
-        language = "Greek"
-    if language not in justext.get_stoplists():
-        language = "English"
-    
-    input_file.seek(0)
-    paragraphs = justext.justext(input_file.read(), justext.get_stoplist(language))
-    
-    for paragraph in paragraphs:
-        output_file.write("<p>" + paragraph.text.replace("\n", " ") + "</p>\n")
+    if input_file.read() != " ":
+        input_file.seek(0)
+        language = langid.classify(input_file.read())
+        language = languages.get(alpha2=language[0]).name
+        if "Greek" in language:
+            language = "Greek"
+        if language not in justext.get_stoplists():
+            language = "English"
+        
+        input_file.seek(0)
+        paragraphs = justext.justext(input_file.read(), justext.get_stoplist(language))
+        
+        for paragraph in paragraphs:
+            output_file.write("<p>" + paragraph.text.replace("\n", " ") + "</p>\n")
+    else:
+        output_file.write(" ")
 
 
 def main():
-    extract("../../Corpus_detourage/Corpus_detourage/clean/JT/", "../../Corpus_detourage/Corpus_detourage/clean/JT_langid/", jt_langid_treatement)
+    extract("../../../resources/JT/", "../../../resources/JT_langid/", jt_langid_treatement)
 
 if __name__ == '__main__':
     main()
--- TD1/exo2/true_lg.py
+++ TD1/exo2/true_lg.py
@@ -4,20 +4,24 @@ import os
 from utils.extractor import extract
 
 def jt_truelg_treatement(input_file, output_file, file_name):
-    languages = json.load(open("../../doc_lg.json"))
-    
-    language = languages[os.path.basename(file_name)]
-    if language not in justext.get_stoplists():
-        language = "English"
-    
-    paragraphs = justext.justext(input_file.read(), justext.get_stoplist(language))
-    
-    for paragraph in paragraphs:
-        output_file.write("<p>" + paragraph.text.replace("\n", " ") + "</p>\n")
+    if input_file.read() != " ":
+        input_file.seek(0)
+        languages = json.load(open("../../../resources/doc_lg.json"))
+        
+        language = languages[os.path.basename(file_name)]
+        if language not in justext.get_stoplists():
+            language = "English"
+        
+        paragraphs = justext.justext(input_file.read(), justext.get_stoplist(language))
+        
+        for paragraph in paragraphs:
+            output_file.write("<p>" + paragraph.text.replace("\n", " ") + "</p>\n")
+    else:
+        output_file.write(" ")
 
 
 def main():
-    extract("../../Corpus_detourage/Corpus_detourage/clean/JT/", "../../Corpus_detourage/Corpus_detourage/clean/JT_trueLg/", jt_truelg_treatement)
+    extract("../../../resources/JT/", "../../../resources/JT_trueLg/", jt_truelg_treatement)
 
 if __name__ == '__main__':
     main()
